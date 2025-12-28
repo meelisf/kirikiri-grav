e4a7716c0e8a2aa91c7f1fbae2ac1896cd3f9cf6
meelisf <meelis.friedenthal@ut.ee>
2025-12-29 01:01:40 +0200

    Enhance Admin UX: Add 'New Post' modal, auto-fields, print styles & image logic

---
+++ user/pages/02.blog/07.katsetus/item.et.md
@@ -0,0 +1,7 @@
+---
+title: Katsetus
+date: '29-12-2025 00:25'
+image: regilaulukaaned.webp
+---
+
+See on uue postituse lisamise katsetus adminni moodulis.
--- user/themes/kirikiri/kirikiri.php
+++ user/themes/kirikiri/kirikiri.php
@@ -6,4 +6,8 @@ use Grav\Common\Theme;
 class Kirikiri extends Theme
 {
     // Access plugin events in this class
+    public static function getCurrentDate()
+    {
+        return date('d-m-Y H:i');
+    }
 }
